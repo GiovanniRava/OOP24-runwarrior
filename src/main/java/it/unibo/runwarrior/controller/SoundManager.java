e5d1f5a65a0a0c9796be0bab81a84fc33e0be397
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-11-25 14:41:36 +0100

    implemented SoundManager

--- src/main/java/it/unibo/runwarrior/controller/SoundManager.java
+++ src/main/java/it/unibo/runwarrior/controller/SoundManager.java
@@ -1,5 +1,42 @@
 package it.unibo.runwarrior.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unibo.runwarrior.view.GameMusic;
+
+/**
+ * Manager to handle all the sounds of the game.
+ */
 public class SoundManager {
-    
+    private static List<GameMusic> ALL_SOUNDS = new ArrayList<>();
+
+    /**
+     * Constructor of the class.
+     */
+    private SoundManager() {
+        //void
+    }
+
+    /**
+     * Create a new instance of GameMusic and add it to the list.
+     *
+     * @param musicFile the name of the music file to load.
+     * @return the created GameMusic object.
+     */
+    public static GameMusic create(final String musicFile) {
+        final GameMusic sound = new GameMusic(musicFile);
+        ALL_SOUNDS.add(sound);
+        return sound;
+    }
+
+    /**
+     * Close all registered GameMusic objects.
+     */
+    public static synchronized void closeAll() {
+        for (final GameMusic sound : ALL_SOUNDS) {
+            sound.close();
+        }
+        ALL_SOUNDS.clear();
+    }
 }
